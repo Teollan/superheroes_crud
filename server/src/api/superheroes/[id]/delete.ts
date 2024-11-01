d2ec3defa7830610dd69cb4f182f3b1b15f7ca12
Teollan <serhiikofficial@gmail.com>
2024-11-01 18:31:35 +0200

    added image clering on complete delete

--- server/src/api/superheroes/[id]/delete.ts
+++ server/src/api/superheroes/[id]/delete.ts
@@ -2,8 +2,9 @@ import { Express } from "express";
 import { INTERNAL_SERVER_ERROR, NOT_FOUND, OK } from "../../../lib/codes";
 import { Error } from "../../../lib/types";
 import { Pool } from "pg";
+import { Bucket } from "@google-cloud/storage";
 
-export default function setup(app: Express, pool: Pool) {
+export default function setup(app: Express, pool: Pool, bucket: Bucket) {
   app.delete("/api/superheroes/:id", async (req, res) => {
     const { id } = req.params;
 
@@ -17,7 +18,17 @@ export default function setup(app: Express, pool: Pool) {
         res.status(NOT_FOUND).json({ message: "Superhero not found" });
       }
 
-      res.status(OK).json(result.rows[0]);
+      const deleted = result.rows[0];
+
+      await Promise.all(
+        deleted.images.map((image: string) => {
+          const fileName = image.split(`${bucket.name}/`)[1];
+
+          return bucket.file(fileName).delete();
+        })
+      );
+
+      res.status(OK).json(deleted);
     } catch (e) {
       const error = e as Error;
 
