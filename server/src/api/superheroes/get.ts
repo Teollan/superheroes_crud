3845e14a207b9e58b444cf3d9b4bccf01525b438
Teollan <serhiikofficial@gmail.com>
2024-11-01 13:33:09 +0200

    added paginated superheroes get

--- server/src/api/superheroes/get.ts
+++ server/src/api/superheroes/get.ts
@@ -5,12 +5,26 @@ import { Pool } from "pg";
 
 export default function setup(app: Express, pool: Pool) {
   app.get("/api/superheroes", async (req, res) => {
-    const { page = 1 } = req.query;
+    const { page = "1", perpage = "5" } = req.query;
 
     try {
-      const result = await pool.query("SELECT * FROM superheroes ORDER BY id");
+      const count = await pool.query(
+        "SELECT COUNT(*) AS count FROM superheroes"
+      );
 
-      res.status(OK).json(result.rows);
+      const result = await pool.query(
+        "SELECT * FROM superheroes ORDER BY id LIMIT $2 OFFSET ($1 - 1) * $2",
+        [page, perpage]
+      );
+
+      res.status(OK).json({
+        info: {
+          page: Number(page),
+          perpage: Number(perpage),
+          total: Number(count.rows[0].count),
+        },
+        results: result.rows,
+      });
     } catch (e) {
       const error = e as Error;
 
